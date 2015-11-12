c16a701e3519b35defc0e149168c5d9240615de6
Stefan Koshy <noreply@skoshy.com>
2015-11-11 23:07:53 -0500

    Gets playlists and playlist tracks

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $http, $ionicModal, $cordovaOauth, $timeout) {
+.controller('AppCtrl', function($scope, $q, $http, $ionicModal, $cordovaOauth, $timeout, dataFact) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -9,15 +9,11 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  // Form data for the login modal
-  var spotify_permissions = [
-    'playlist-read-private'
-    ,'playlist-read-collaborative'
-    ,'playlist-modify-public'
-    ,'playlist-modify-private'
-    ,'user-library-read'
-    ,'user-library-modify'
-  ];
+  $scope.user = dataFact.user;
+  $scope.appData = dataFact.appData;
+
+  // Debug
+  appData = dataFact.appData;
 
   $scope.spotify = {
     uri: 'https://accounts.spotify.com/authorize'
@@ -41,15 +37,17 @@ angular.module('starter.controllers', [])
     $scope.modal = modal;
   });
 
+  console.log(dataFact.getData('access_token'));
+
+  /****
+  ===== Important Functions
+  ****/
+
   // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+  $scope.closeLogin = function() { $scope.modal.hide(); };
 
   // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+  $scope.login = function() { $scope.modal.show(); };
 
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
@@ -57,42 +55,30 @@ angular.module('starter.controllers', [])
 
     // Simulate a login delay. Remove this and replace with your login
     // code if using a login system
-    $cordovaOauth.spotify($scope.spotify.client_id, $scope.spotify.scope).then(function(result) {
+    $cordovaOauth.spotify($scope.spotify.client_id, $scope.spotify.scope, {show_dialog: true}).then(function(result) {
         // Access token: result.access_token
         // Expires in: result.expires_in
-        window.localStorage['access_token'] = result.access_token;
+        dataFact.setData('access_token', result.access_token);
         
-        // Get a user's ID
-        $http({
-          method: 'GET'
-          ,headers: {
-            'Authorization': 'Bearer ' + window.localStorage['access_token']
-          }
-          ,url: 'https://api.spotify.com/v1/me'
-        }).then(function successCallback(response) {
-          // this callback will be called asynchronously
-          // when the response is available
-          console.log(JSON.stringify(response));
-        }, function errorCallback(response) {
-          // called asynchronously if an error occurs
-          // or server returns response with an error status.
-        });
+        dataFact.afterLogin();
     }, function(error) {
         console.log(error);
     });
   };
+
+  if (dataFact.getData('access_token') != '')
+    dataFact.afterLogin();
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
+.controller('PlaylistsCtrl', function($scope, dataFact) {
+  $scope.user = dataFact.user;
+  $scope.appData = dataFact.appData;
 })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
+.controller('PlaylistCtrl', function($scope, $stateParams, dataFact) {
+  $scope.user = dataFact.user;
+  $scope.appData = dataFact.appData;
+  $scope.playlistId = $stateParams.playlistId;
+
+  var playlistData = dataFact.getPlaylist(0, $stateParams.playlistId);
 });
--- www/js/services.js
+++ www/js/services.js
@@ -1,9 +1,98 @@
-angular.module('starter.controllers', [])
+angular.module('starter.services', [])
 
-.factory('webServices', function($scope, $http) {
+.factory('dataFact', function($http, $q) {
+	var user = {display_name: 'Not logged in'};
+	var appData = {};
+	appData.playlists = [];
+	appData.playlistTracks = [];
+
+	var storage = window.localStorage;
+
+	function setData(key, data) {
+		storage[key] = data;
+	}
+
+	function getData(key) {
+		return storage[key];
+	}
+
+	function authHttpHeader() {
+	    return 'Bearer ' + window.localStorage['access_token'];
+	}
+
+	// Synchronously does a HTTP request. Will return a promise object
+	function requestSynch(method, url, authNeeded, params) {
+		// Determine if we need to auth or not
+		if (authNeeded) var headers = { 'Authorization': authHttpHeader() };
+		else var headers = {};
+
+		if (params == undefined) params = {};
+
+		var deferred = $q.defer();
+
+		// Make the call
+		$http({ method: method, headers: headers ,url: url, params: params})
+			.then(function successCallback(response) {
+				deferred.resolve(response);
+			}, function errorCallback(response) {
+				deferred.reject(response);
+		});
+
+		return deferred.promise;
+	}
+
+	function getPlaylist(offset, playlistId) {
+		promise = requestSynch('GET', 'https://api.spotify.com/v1/users/'+user.id+'/playlists/'+playlistId+'/tracks', true, {limit: 100, offset: offset});
+		promise.then(function(response){
+			console.log(response);
+
+			if (appData.playlistTracks[playlistId] === undefined)
+				appData.playlistTracks[playlistId] = response.data.items;
+			else
+				appData.playlistTracks[playlistId] = appData.playlistTracks[playlistId].concat(response.data.items);
+
+			console.log(appData.playlistTracks);
+
+			if (response.data.total > appData.playlistTracks[playlistId].length)
+				getPlaylist(appData.playlistTracks[playlistId].length, playlistId);
+		});
+	}
+
+	function getAllPlaylists(offset) {
+		promise = requestSynch('GET', 'https://api.spotify.com/v1/users/'+user.id+'/playlists', true, {limit: '50', offset: offset});
+		promise.then(function(response){
+			appData.playlists = appData.playlists.concat(response.data.items);
+
+			if (response.data.total > appData.playlists.length)
+				getAllPlaylists(appData.playlists.length);
+		});
+	}
+
+	function afterLogin() {
+		// Get user's data
+		promise = requestSynch('GET', 'https://api.spotify.com/v1/me', true);
+		promise.then(function(response) {
+		  // Set user's name and ID
+		  console.log(JSON.stringify(response));
+		  user.display_name = response.data.display_name;
+		  user.id = response.data.id;
+
+		  // Get user's playlists
+		  getAllPlaylists(0);
+		}, function(response) {
+			// Didn't authenticate
+			console.log('Authentication failed, try again');
+			storage['access_token'] = '';
+		});
+	}
+
 	return {
-		'loginSpotify': function() {
-			return 
-		}
+		user: user
+		,appData: appData
+
+		,getData: getData
+		,setData: setData
+		,afterLogin: afterLogin
+		,getPlaylist: getPlaylist
 	};
 });
